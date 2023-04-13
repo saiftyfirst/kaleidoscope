0e562a695a0d19ef0bc3c3af0ef1507dea69b08f
saifahmed <saif.ahmed@imc.com>
2023-04-13 22:59:22 +0200

    Add skeleton for LLVM Generator

--- src/codegen/generator.rs
+++ src/codegen/generator.rs
@@ -1,31 +1,5 @@
-use crate::syntax::ast;
 use crate::syntax::ast::GenericAst;
 
 pub trait Generator<T> {
-
-//     NumberExprAst { number: f64 },
-// VariableExprAst { name: String },
-// BinaryExprAst { op: char, lhs: Box<GenericAst>, rhs: Box<GenericAst> },
-// CallExprAst { callee: String, args: Vec<GenericAst> },
-// PrototypeAst { name: String, args: Vec<String> },
-// FunctionAst { proto: Box<GenericAst>, body: Box<GenericAst> }
-
-    unsafe fn generate(&self, ast: &GenericAst) -> T {
-        match ast {
-            GenericAst::FunctionAst { proto, body } => {
-                let _context = llvm_sys::core::LLVMGetGlobalContext();
-                unsafe { llvm_sys::core::LLVMIntType(32) }
-            }
-            _ => {
-                let _context = llvm_sys::core::LLVMGetGlobalContext();
-                unsafe { llvm_sys::core::LLVMIntType(32) }
-            }
-        }
-    }
-
-
-
-
-
-
+    unsafe fn generate(&self, ast: &GenericAst) -> T;
 }
--- src/codegen/llvm_generator.rs
+++ src/codegen/llvm_generator.rs
@@ -1,9 +1,11 @@
-use crate::syntax::ast;
+use llvm_sys::prelude::LLVMTypeRef;
+
+use crate::codegen::generator::Generator;
 use crate::syntax::ast::GenericAst;
 
-pub trait Generator {
+impl Generator<LLVMTypeRef> for GenericAst {
     /*
-        Learning Note:
+        Learning Notes:
         - Single Static Assignment (SSA)
             SSA is a form of intermediate representation (IR) used in compilers.
             SSA specifies that each variable is assigned only once, and that each assignment is a definition of the variable.
@@ -11,50 +13,12 @@ pub trait Generator {
             Versioning is used to keep track of the different values of a variable.
             In other words, there is no way to change an SSA value.
     */
-
-//     NumberExprAst { number: f64 },
-// VariableExprAst { name: String },
-// BinaryExprAst { op: char, lhs: Box<GenericAst>, rhs: Box<GenericAst> },
-// CallExprAst { callee: String, args: Vec<GenericAst> },
-// PrototypeAst { name: String, args: Vec<String> },
-// FunctionAst { proto: Box<GenericAst>, body: Box<GenericAst> }
-
-    unsafe fn generate(&self, ast: &GenericAst) -> llvm_sys::prelude::LLVMTypeRef {
+    unsafe fn generate(&self, ast: &GenericAst) -> LLVMTypeRef {
         match ast {
-            GenericAst::FunctionAst { proto, body } => {
-                let _context = llvm_sys::core::LLVMGetGlobalContext();
-                unsafe { llvm_sys::core::LLVMIntType(32) }
-            }
             _ => {
                 let _context = llvm_sys::core::LLVMGetGlobalContext();
                 unsafe { llvm_sys::core::LLVMIntType(32) }
             }
         }
     }
-
-
-
-
-
-
 }
-
-// impl GenericAst {
-//     /*
-//         Learning Note:
-//         - Single Static Assignment (SSA)
-//             SSA is a form of intermediate representation (IR) used in compilers.
-//             SSA specifies that each variable is assigned only once, and that each assignment is a definition of the variable.
-//             Existing variables are not modified, but new variables are created to hold the results of expressions.
-//             Versioning is used to keep track of the different values of a variable.
-//             In other words, there is no way to change an SSA value.
-//     */
-//     pub unsafe fn codegen(&self) -> llvm_sys::prelude::LLVMTypeRef {
-//         match self{
-//             _ => {
-//                 let _context = llvm_sys::core::LLVMGetGlobalContext();
-//                 unsafe { llvm_sys::core::LLVMIntType(32) }
-//             }
-//         }
-//     }
-// }
