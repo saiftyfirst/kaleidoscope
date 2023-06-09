9d1e363ff3af99e91c736000d361a34236248e4e
saifahmed <saif.ahmed@imc.com>
2023-06-09 22:07:17 +0200

    Add driver code to interact with JIT compiler

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Read, Write};
+use kaleidoscope::parse::parser::*;
+
+const QUIT_CMD : &str = "quit";
+
+pub struct Driver {}
+
+impl Driver {
+    pub fn run() {
+        loop {
+            print!("ready>> ");
+            io::stdout().flush().unwrap(); // flushes the buffer
+
+            let mut prompt = String::new();
+            io::stdin().read_line(&mut prompt).unwrap();
+
+            if prompt.trim() == QUIT_CMD {
+                break;
+            }
+
+            let mut parser = Parser::new(&prompt);
+            println!("{}", parser.build_next_ast().unwrap());
+        }
+    }
+}
+
+fn main() {
+    Driver::run();
+}
