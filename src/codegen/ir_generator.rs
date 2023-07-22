95089ef966d5f285be94b67f06569af43aad6574
sahmed <System.Administrator@imc.com>
2023-07-22 22:38:43 +0200

    Add new framework for IR Generation

--- src/codegen/ir_generator.rs
+++ src/codegen/ir_generator.rs
@@ -1,3 +1,27 @@
+use std::fmt::Error;
+
+// legacy
 pub trait IRGenerator<C, T> {
     unsafe fn generate(&self, context: &mut C) -> T;
+}
+
+pub trait CodeGenerator {
+    type Item;
+    type AstContainer: IntoIterator<Item=Self::Item>;
+
+    fn generate(&self, container: Self::AstContainer) -> Result<(), Error>;
+}
+
+pub struct Compiler<Generator: CodeGenerator>{
+    generator: Generator
+}
+
+impl<Generator: CodeGenerator> Compiler<Generator> {
+    pub fn new(generator: Generator) -> Self {
+        Compiler { generator }
+    }
+
+    pub fn compile(&self, asts: Generator::AstContainer) -> Result<(), Error> {
+        self.generator.generate(asts)
+    }
 }
